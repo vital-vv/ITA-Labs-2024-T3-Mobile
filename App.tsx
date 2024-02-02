4453d70c0112389b22579d8b0367b4b58c9e0182
Khomtchik Anastasiya <lawrik.nastik@gmail.com>
2024-02-02 10:42:29 +0300

    feat: fetch, styles, loading

--- App.tsx
+++ App.tsx
@@ -1,47 +1,12 @@
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 
-import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
-
-import { LotViewScreen } from './screens/LotView/LotView'
+import LotViewScreen from './screens/LotView/LotView';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-  const styles = StyleSheet.create({
-    sectionContainer: {
-      marginTop: 32,
-      paddingHorizontal: 24,
-    },
-    sectionTitle: {
-      fontSize: 24,
-      fontWeight: '600',
-    },
-    sectionDescription: {
-      marginTop: 8,
-      fontSize: 18,
-      fontWeight: '400',
-    },
-    highlight: {
-      fontWeight: '700',
-    },
-  });
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar/>
-      <ScrollView>
-        <LotViewScreen />
-      </ScrollView>
+    <SafeAreaView>
+      <StatusBar />
+      <LotViewScreen />
     </SafeAreaView>
   );
 }
---
+++ components/buttons/ButtonWithIcon/ButtonWithIcon.tsx
@@ -0,0 +1,21 @@
+import {Image, Text, Pressable} from 'react-native';
+
+import styles from './buttonWithIconStyle';
+
+const ButtonWithIcon = (props: {
+  title: string;
+  type: 'light' | 'dark';
+  image: object;
+}) => {
+  return (
+    <Pressable style={props.type === 'light' ? styles.light : styles.dark}>
+      <Image style={styles.image} source={props.image} />
+      <Text
+        style={props.type === 'light' ? styles.title_light : styles.title_dark}>
+        {props.title}
+      </Text>
+    </Pressable>
+  );
+};
+
+export default ButtonWithIcon;
